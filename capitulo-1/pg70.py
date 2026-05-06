a2b269cb544721ecdf9e396943860db2de58bc61
leandro-henrique09 <leandrohdc09@gmail.com>
2026-05-05 22:01:22 -0300

    criação de pastas e pg 70 e pg 72

---
+++ capitulo-1/pg70.py
@@ -0,0 +1,36 @@
+# Operadores matemáticos
+
+"+"    =   Adição
+# 5 + 2 = 3
+
+
+
+"-"    =   Subtração
+# 5 - 3 = 2
+
+
+
+"*"    =   Mutiplicação
+# 2 * 10 = 20
+
+
+
+"/"    =   Divisão(com resultado fracionario)
+# 20 / 4 = 5.0
+# 10 / 4 = 2.5
+
+
+
+"//"   =   Divisão(com resultados inteiros)
+# 10 // 4 = 2
+
+
+
+"%"    =   Módulo ou resto 
+# 10 % 3 = 1
+# 16 % 7 = 2
+# 63 & 8 = 7
+
+
+"**"   =   Exponenciação ou potenciação
+# 2 ** 3 = 8
---
+++ capitulo-1/pg72.py
@@ -0,0 +1,4 @@
+# Primeiro programa com variáveis
+a = 2 # a variável a recebe o "2" como valor.
+b = 3 # a variável b recebe o "3" como valor.
+print(a + b) # a função print realiza a impressão da soma a + b
